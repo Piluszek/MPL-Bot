2fee9349f9d43dd617a92faff4ab09906c4814a8
Piluszek <gro.soltys@gmail.com>
2026-06-21 03:16:34 +0200

    0.2

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -26,21 +26,49 @@ int main() {
 			int temp_min = (int)(lap_time.at(0)- '0');									// wzięcie minut 
 			double lap = std::stof(lap_time.substr(lap_time.find('.'))) + temp_min * 60;	// wyjęcie sekund i milisekund oraz dodanie minut zamienione na sec
 
-			float number_laps = length / lap;
+			int buffor_laps = 0;
+			if (event.get_parameter("bufor_laps").index() != 0) {				// pobieram zużycie energii jeśli użytkownik podał i tutaj obliczam ilość stintów
+				buffor_laps = std::get<int64_t>(event.get_parameter("bufor_laps"));
+			}
+			float number_laps = (length / lap) + buffor_laps;
 			int number_laps_int = std::ceil(number_laps);
 
-
+			int energy_usage = 0;
 			int number_stints = 0;
 			if (event.get_parameter("energy_usage").index() != 0) {				// pobieram zużycie energii jeśli użytkownik podał i tutaj obliczam ilość stintów
-				int energy_usage = std::get<int64_t>(event.get_parameter("energy_usage"));
+				energy_usage = std::get<int64_t>(event.get_parameter("energy_usage"));
 
-				number_stints =std::ceil(number_laps_int / (int)(100 / energy_usage));
+				number_stints =std::ceil(number_laps_int / (100 / energy_usage));
 
 			}
 			else {
 				
-				number_stints = std::ceil(number_laps_int / (int)(tank / fuel_usage));
+				number_stints = std::ceil(number_laps_int / (tank / fuel_usage));
+
 			}
+
+			std::vector<float> fuel_stint;				
+			int race_fuel = number_laps_int * fuel_usage;		// obliczenie ile paliwa na cały wyścig
+			if (race_fuel > tank) {
+				int temp_fuel = 0;
+				for (int i = 0; i < number_stints - 1; i++) {
+					fuel_stint.push_back(tank);
+					temp_fuel += tank;
+				}
+				fuel_stint.push_back(race_fuel - temp_fuel);
+				if ((fuel_stint[fuel_stint.size() - 1] * 100) / fuel_stint[fuel_stint.size() - 2] < 40) {
+					int temp = (fuel_stint[fuel_stint.size() - 1] + fuel_stint[fuel_stint.size() - 2]) / 2;
+					fuel_stint[fuel_stint.size() - 1] = temp;
+					fuel_stint[fuel_stint.size() - 2] = temp;
+				}
+			}
+			else {
+				fuel_stint.push_back(race_fuel);
+			}
+
+
+
+
 
 
 			{													// ustawienia embed
@@ -68,10 +96,17 @@ int main() {
 					number_stints_string
 				);
 
-				embed.add_field(
-					"Stint 1:",
-					"0"
-				);
+				if (energy_usage == 0) {
+					for (int i = 0; i <= fuel_stint.size()-1; i++) {
+						std::string fuel_string = std::to_string(fuel_stint[i]);
+						std::string tekst = (fuel_string).substr(0, fuel_string.find('.') + 2) + " L"; // tutaj tymaczasowo zaokrągla w górę !!!!
+						std::string tekst2 = "Stint " + std::to_string(i+1)+":";
+						embed.add_field(
+							tekst2,
+							tekst
+						);
+					}
+				}
 
 			}
 
@@ -115,6 +150,7 @@ int main() {
 			calculator.add_option(dpp::command_option(dpp::co_integer, "tank", "pojemnosc zbiornika paliwa (l)", true));
 			calculator.add_option(dpp::command_option(dpp::co_number, "fuel_usage", "zuzycie paliwa l/lap", true));
 			calculator.add_option(dpp::command_option(dpp::co_integer, "energy_usage", "zuzycie energii (%/lap)", false));
+			calculator.add_option(dpp::command_option(dpp::co_integer, "bufor_laps", "ile lapow zapasu", false));
 			
 			commands.push_back(calculator);
 			
